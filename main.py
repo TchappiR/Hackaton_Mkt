a20a939cb14b8422449b0a9e3621893bb4f1b71e
TchappiR <tchappiromaric@gmail.com>
2024-12-03 01:24:36 +0100

    delete cot important columns

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ import numpy as np
 # preparation de la data base
 
 data_base = pd.read_csv("database.csv")
+print(data_base.shape())
 print(data_base.info())
+
+# Nettoyage de la base de donnée
+
+data_clean = data_base.drop(["ID","Education", "Marital_Status", "Year_Birth", "Z_CostContact", "Complain", "Dt_Customer"], axis=1)
+
+
+
