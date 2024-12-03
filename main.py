222ccc24929ae3b7f598cbbfe45eab663daf3ee1
TchappiR <tchappiromaric@gmail.com>
2024-12-03 01:52:39 +0100

    change dtype

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ data_base = pd.read_csv("database.csv")
 
 # Nettoyage de la base de donnée
 
-data_clean = data_base.drop(["ID","Education", "Marital_Status", "Year_Birth", "Z_CostContact", "Complain", "Dt_Customer"], axis=1)
+data_clean = data_base.drop(["ID","Education", "Marital_Status", "Year_Birth", "Z_CostContact", "Complain"], axis=1)
 
 # Gestion des valeurs manquantes
 
@@ -20,6 +20,12 @@ data_clean.dropna(inplace= True)
 
 (data_clean.isnull().sum(axis=0))
 (data_clean.info())
+
+# Changement des types
+
+data_clean["Dt_Customer"] = pd.to_datetime(data_clean["Dt_Customer"], errors='coerce')
+(data_clean.info())
+
 
 
 
